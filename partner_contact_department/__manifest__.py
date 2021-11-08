a54eceaaad4091792b0915dbf0ba316d3c60b50c
Janik von Rotz <login@janikvonrotz.ch>
2021-11-08 20:14:56 +0100

    set different version to override oca module

--- partner_contact_department/__manifest__.py
+++ partner_contact_department/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Mint System GmbH, Odoo Community Association (OCA)',
     'website': 'https://www.mint-system.ch',
     'category': 'Customer Relationship Management',
-    'version': '14.0.1.0.0',
+    'version': '14.1.1.0.0',
     'license': 'AGPL-3',
     
     'depends': ['contacts'],
