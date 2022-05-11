d8033ba587475c71ac9f5641a21bf9afa43db9ab
janikvonrotz <contact@janikvonrotz.ch>
2022-05-11 15:44:55 +0200

    [partner_type_order] init

---
+++ partner_type_order/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Partner Type Order",
+    "summary": """
+        Adds new partner type order address.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Administration",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/view_partner_form.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
