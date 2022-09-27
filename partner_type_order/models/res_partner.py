344173b29ef8bdbfbef464034e362e464e054d93
janikvonrotz <contact@janikvonrotz.ch>
2022-09-27 15:22:40 +0200

    [partner_type_order] show type in get name [partner_type_sale] show type in get name

--- partner_type_order/models/res_partner.py
+++ partner_type_order/models/res_partner.py
@@ -1,15 +1,25 @@
 from odoo import fields, models
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
-    _inherit = "res.partner"
+    _inherit = 'res.partner'
 
     type = fields.Selection(selection_add=[
         ('order', 'Order Address')
     ], ondelete={'contact': 'set default'})
 
     def get_address_default_type(self):
-        """Add new order type"""
+        """Add new order type."""
         res = super().get_address_default_type()
-        res.add("order")
+        res.add('order')
         return res
+
+    def _get_name(self):
+        partner = self
+        name = super(ResPartner, self)._get_name()
+        if partner.company_name or partner.parent_id:               
+            if not partner.name and partner.type in ['order']:
+                name += dict(self.fields_get(['type'])['type']['selection'])[partner.type]
+        return name
