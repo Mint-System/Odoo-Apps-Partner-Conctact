e7fa0f0d219b0e25afb27e0a49d4971d2c6a27dc
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-05-03 17:05:34 +0200

    feat: upgrade partner_contact_department_note

---
+++ partner_contact_department_note/models/res_partner.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    department = fields.Char(string="Department")
