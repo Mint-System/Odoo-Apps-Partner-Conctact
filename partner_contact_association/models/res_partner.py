07d889ce731c3bad4d2182d171c5ed23c5d4cb16
Janik von Rotz <login@janikvonrotz.ch>
2024-06-14 16:50:21 +0200

    feat: show and search association name

--- partner_contact_association/models/res_partner.py
+++ partner_contact_association/models/res_partner.py
@@ -1,11 +1,33 @@
 import logging
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
+
+from odoo.osv import expression
 
 
 class Partner(models.Model):
     _inherit = "res.partner"
 
     association_id = fields.Many2one("res.association")
+    association_name = fields.Char(
+        related="association_id.name", string="Assocation Name", store=True
+    )
+
+    def _get_name(self):
+        """Show association name is displayname."""
+        partner = self
+        name = super(Partner, self)._get_name()
+        if partner.association_id:
+            name = "%s, %s" % (partner.association_id.name, name)
+        return name
+
+    @api.model
+    def name_search(self, name="", args=None, operator="ilike", limit=100):
+        """Always search in association name."""
+        if args is None:
+            args = []
+        args = expression.OR([args, [("association_name", operator, name)]])
+        _logger.warning(args)
+        return super().name_search(name=name, args=args, operator=operator, limit=limit)
