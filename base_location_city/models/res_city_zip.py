20e0c1993f3e9513c23d11908dd3238d42d3fac2
Janik von Rotz <login@janikvonrotz.ch>
2024-08-23 14:42:47 +0200

    feat: add res.city.zip

--- base_location_city/models/res_city_zip.py
+++ base_location_city/models/res_city_zip.py
@@ -1,8 +1,8 @@
 from odoo import fields, models
 
 
-class ResCity(models.Model):
-    _inherit = "res.city"
+class ResCityZip(models.Model):
+    _inherit = "res.city.zip"
 
     latitude = fields.Float(digits=(16, 13))
     longitude = fields.Float(digits=(16, 13))
