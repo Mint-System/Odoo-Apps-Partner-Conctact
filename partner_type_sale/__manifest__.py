2aa909a85c058a468c467f768248f48e4ed1a4b4
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-07 10:54:36 +0100

    feat: init partner_type_sale migration

---
+++ partner_type_sale/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Partner Type Sale",
+    "summary": """
+        Adds new partner type sale contact address.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Administration",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
