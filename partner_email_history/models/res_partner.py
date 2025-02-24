78c0842da659042c14c6be51c74ff0aff7d36126
Janik von Rotz <login@janikvonrotz.ch>
2025-02-24 16:57:31 +0100

    feat(partner_email_history): domain filter

--- partner_email_history/models/res_partner.py
+++ partner_email_history/models/res_partner.py
@@ -1,27 +1,26 @@
 import logging
 
 from odoo import models
-from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
 
 
-class Parnter(models.Model):
+class Partner(models.Model):
     _inherit = "res.partner"
 
     def action_show_email_history(self):
-        action = self.env.ref("mail.action_view_mail_message")
-        result = action.read()[0]
+        action = self.env.ref("mail.action_view_mail_message").read()[0]
         subtype_id = self.env.ref("mail.mt_comment")
-        result["domain"] = expression.AND(
-            [
-                [("subtype_id", "=", subtype_id.id)],
-                expression.OR(
-                    [
-                        [("email_from", "ilike", self.email)],
-                        [("partner_ids", "=", self.email)],
-                    ]
-                ),
-            ]
-        )
-        return result
+        message_type = "email"
+        action["domain"] = [
+            "&",
+            "|",
+            ("subtype_id", "=", subtype_id.id),
+            ("message_type", "=", message_type),
+            "|",
+            ("author_id", "ilike", self.email),
+            "|",
+            ("email_from", "ilike", self.email),
+            ("partner_ids", "ilike", self.email),
+        ]
+        return action
